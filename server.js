5ff48138502e073efdc278214acc09e3a0c4ad81
Glitch (ctftime) <none>
2024-06-22 05:13:52 +0000

    🗣️⚙️ Updated with Glitch

--- server.js
+++ server.js
@@ -4,15 +4,21 @@ const cheerio = require("cheerio");
 
 const app = express();
 
-app.get("/team/:id", async (req, res) => {
+app.get("/api/team/:id", async (req, res) => {
   const id = req.params.id;
   const url = "https://ctftime.org/team/" + id;
   const response = await fetch(url);
   const body = await response.text();
   const $ = cheerio.load(body);
   const data = {};
+  
+  // General stuff
   data.id = id;
   data.team_name = $("h2").text().substring(1);
+  data.pfp = $(".span2 > img").attr("src");
+  data.description = $(".span12").find("p").text();
+
+  // Global stuff
   var global_rank = $("p:contains('Overall rating place:')")
     .text()
     .replace(/\s+/g, " ")
@@ -27,6 +33,7 @@ app.get("/team/:id", async (req, res) => {
     global_rank.indexOf("pts") - 1
   );
 
+  // Per country stuff
   var country_rank = $("p:contains('Country place:')")
     .text()
     .replace(/\s+/g, " ")
@@ -41,14 +48,46 @@ app.get("/team/:id", async (req, res) => {
         .attr("href")
         .indexOf("/stats/") + 7
     );
+
+  // Competition stuff
   data.competitions = [];
-  var competitions = $(".table-striped")[0];
-  var compHeaders = competitions.children("tr")[0]
-  for (var i = 1;i<competitions.children("tr").length;i++) {
-    var event = {}
-    const row = competitions.children("tr")[i];
+  var competitions = $("table").eq(0);
+  var compHeaders = competitions.children("tr").eq(0).children("th");
+  for (var i = 1; i < competitions.children("tr").length; i++) {
+    var event = {};
+    const row = competitions.children("tr").eq(i).children("td");
+    for (var a = 1; a < row.length; a++) {
+      event[
+        compHeaders
+          .eq(a - 1)
+          .text()
+          .toLowerCase()
+          .replace(/\s+/g, "_")
+          .trim()
+      ] = row.eq(a).text();
+    }
+    data.competitions.push(event);
   }
 
+  // Members stuff
+  data.current_members = [];
+  var current_members = $("table").eq(1).children("tr");
+  for (var i = 0; i < current_members.length; i++) {
+    var user = {};
+    user.name = current_members.eq(i).children("td").text();
+    user.id = current_members.eq(i).find("a").attr("href").substring(6);
+    data.current_members.push(user);
+  }
+  data.former_members = [];
+  var former_members = $("table").eq(2).children("tr");
+  for (var i = 0; i < former_members.length; i++) {
+    var user = {};
+    user.name = former_members.eq(i).children("td").text();
+    user.id = former_members.eq(i).find("a").attr("href").substring(6);
+    data.former_members.push(user);
+  }
+
+  // Send it to the user!
   res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify(data, null, 4));
 });
